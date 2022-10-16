afb34bf134ccf8f4472255a886a4b7fa6eb54258
jia1314hu <115885345+jia1314hu@users.noreply.github.com>
2022-10-16 08:14:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx14e9d82ae7fe7bf0',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '11f93fb21c23aed01b735fde5d14db3d',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'ol6IX6q-Z_NHthTEma3fWpDRHlXo',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ol6IX6lcdhGe77NTHyl6cPGacsls',
     }
   ],
 
